b9b367fdc562c0859f76a1e050da088726cc8b52
Mohammed Rafi D <rafi04rafi@gmail.com>
2025-01-26 18:55:52 +0530

    brute working

--- backend/server.js
+++ backend/server.js
@@ -26,22 +26,61 @@ db.connect((err) => {
   }
 });
 
+//checking if vehicle exists
 app.post('/api/vehicles', (req,res) =>{
   const sql = " select * from vehicles where numberPlate = ? ";
   db.query(sql, [req.body.number] , (err, data) => {
     if(err){
       return res.status(500).json({ error: 'Database error' }); 
     }
-    if(data.length == 0){
+    if(data.length === 0){
       return res.json('vehicle is not there');
     }else{
+      console.log('vehicle there');
       return res.json('vehicle there');
     }
   });
 });
 
+
+app.post('/api/transactions',(req,res) => {
+  const sql = "select * from transactions where numberPlate = ? " ;
+  
+  // if (!req.body.transData) {
+  //   console.log('first type madu jhaat');
+  //   return res.status(400).json({ error: 'Transaction data is required' });
+  // }
+
+  db.query(sql, [req.body.transData], (err,data) => {
+    if(err){
+      return res.status(500).json({error: 'DataBase error'});
+    }
+    if(data.length === 0){
+      return res.json('no fines');
+    }
+    return res.json(data);
+  });
+});
+
+
+//random complete check of the fines present in the database
+app.get('/api/fines',(req,res) => {
+  db.query("select * from fines",(err, result) => {
+    if(err){
+      console.error('error fetching :', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.json(result);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`backend server is running in this on http://localhost:${port}`)
+});
+
+
 // app.get('/api/vehicles',(req,res) => {
-//   db.query("select * from vehicles",(err, result) => {
+//   db.query("select * from vehicles where numberPlate = ?",[req.body.number],(err, result) => {
 //     if(err){
 //       console.error('error fetching :', err);
 //       return res.status(500).send('Internal Server Error');
@@ -50,15 +89,9 @@ app.post('/api/vehicles', (req,res) =>{
 //   })
 // });
 
-// app.get('/api/fines',(req,res) => {
-//   db.query("select * from fines",(err, result) => {
-//     if(err){
-//       console.error('error fetching :', err);
-//       return res.status(500).send('Internal Server Error');
-//     }
-//     res.json(result);
-//   })
-// });
+
+//example checkings 
+
 
 // app.get('/api/transactions',(req,res) => {
 //   db.query("select * from transactions",(err, result) => {
@@ -69,7 +102,3 @@ app.post('/api/vehicles', (req,res) =>{
 //     res.json(result);
 //   })
 // });
-
-app.listen(port, () => {
-  console.log(`backend server is running in this on http://localhost:${port}`)
-})
